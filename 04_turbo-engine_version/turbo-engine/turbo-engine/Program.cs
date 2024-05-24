7c93211b44892926fa5a24a62c63815bce1b09a7
Luis Renato <luisrenato@gmail.com>
2024-05-24 11:55:29 -0300

    Implementando versionamento

---
+++ 04_turbo-engine_version/turbo-engine/turbo-engine/Program.cs
@@ -0,0 +1,33 @@
+using turbo_engine.Services.Implementations;
+using turbo_engine.Services;
+using Microsoft.EntityFrameworkCore;
+using turbo_engine.Model.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
+
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 04_turbo-engine_version/turbo-engine/turbo-engine/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using turbo_engine.Model;
+
+namespace turbo_engine.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
