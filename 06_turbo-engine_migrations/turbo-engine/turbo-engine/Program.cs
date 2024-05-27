a2043e11bbe4ed41fc405411a9779b77c79dddb6
Luis Renato <luisrenato@gmail.com>
2024-05-27 10:06:21 -0300

    Implementando multicamadas e migrations

---
+++ 06_turbo-engine_migrations/turbo-engine/turbo-engine/Program.cs
@@ -0,0 +1,36 @@
+using turbo_engine.Business.Implementations;
+using turbo_engine.Business;
+using Microsoft.EntityFrameworkCore;
+using turbo_engine.Model.Context;
+using turbo_engine.Repository.Implementations;
+using turbo_engine.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 0, 29)))
+);
+
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ 06_turbo-engine_migrations/turbo-engine/turbo-engine/Repository/IPersonRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using turbo_engine.Model;
+
+namespace turbo_engine.Repository
+{
+    public interface IPersonRepository
+    {
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+        bool Exists(long id);
+        
+    }
+}
