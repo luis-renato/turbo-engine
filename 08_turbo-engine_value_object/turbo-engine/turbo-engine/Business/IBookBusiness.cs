48edf5fa4ab61f1c4602cbf0032475deac9402f2
Luis Renato <luisrenato@gmail.com>
2024-05-29 15:52:53 -0300

    Implementando generic repository

---
+++ 08_turbo-engine_value_object/turbo-engine/turbo-engine/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using turbo_engine.Model;
+using System.Collections.Generic;
+
+namespace turbo_engine.Business
+{
+    public interface IBookBusiness
+    {
+        Book Create(Book book);
+        Book FindByID(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
