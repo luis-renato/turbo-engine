c8af7335934e5dcc4754458a8bcdfebc06c88511
Luis Renato <luisrenato@gmail.com>
2024-05-29 15:54:02 -0300

    Implementando generic repository

---
+++ 07_turbo-engine_generic_repository/turbo-engine/turbo-engine/Business/IPersonBusiness.cs
@@ -0,0 +1,13 @@
+﻿using turbo_engine.Model;
+
+namespace turbo_engine.Business
+{
+    public interface IPersonBusiness
+    { 
+        Person Create(Person person);
+        Person FindByID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
