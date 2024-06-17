31726ed212d6d33a68764802db02f92fadeb4126
Luis Renato <luisrenato@gmail.com>
2024-06-17 11:43:10 -0300

    Hateoas

---
+++ 11_turbo-engine_hateoas/turbo-engine/turbo-engine/HyperMedia/Abstract/ISupportHyperMedia.cs
@@ -0,0 +1,7 @@
+﻿namespace turbo_engine.HyperMedia.Abstract
+{
+    public interface ISupportHyperMedia
+    {
+        List<HyperMediaLink> Links { get; set;  }
+    }
+}
---
+++ 11_turbo-engine_hateoas/turbo-engine/turbo-engine/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using turbo_engine.Model;
+using turbo_engine.Model.Base;
+
+namespace turbo_engine.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
