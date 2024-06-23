f9f08c719655da70c2cb54241f9e22b46d173787
Luis Renato <luisrenato@gmail.com>
2024-06-23 19:01:27 -0300

    Token Authorization, Refresh and Revoke

---
+++ 14_turbo-engine_Authentication/turbo-engine/turbo-engine/Data/VO/BookVO.cs
@@ -0,0 +1,20 @@
+﻿using turbo_engine.HyperMedia;
+using turbo_engine.HyperMedia.Abstract;
+
+namespace turbo_engine.Data.VO
+{
+    public class BookVO : ISupportHyperMedia
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime LaunchDate { get; set; }
+        public List<HyperMediaLink> Links { get; set; } = new List<HyperMediaLink>();
+
+    }
+}
---
+++ 14_turbo-engine_Authentication/turbo-engine/turbo-engine/Data/VO/UserVO.cs
@@ -0,0 +1,8 @@
+﻿namespace turbo_engine.Data.VO
+{
+    public class UserVO
+    {
+        public string Password { get; set; }
+        public string UserName { get; set; }
+    }
+}
